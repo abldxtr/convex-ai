8cd4551f5557c6b78a2c5a48f25e26d310cb0ce7
abol.sa.lej <abol.dexter@gmail.com>
2025-06-04 18:58:58 +0330

    update 6/4

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -85,6 +85,7 @@ export function AppSidebar({ user }: { user: UserType }) {
       toast.success("Chat deleted successfully");
       if (chatIdd === id) {
         router.replace("/chat");
+        setActive(false);
       }
     } catch (error) {
       console.log({ error });
