9a32217a4552e7761eb62a561e8724d1606e3d88
abol.sa.lej <abol.dexter@gmail.com>
2025-06-12 08:40:21 +0330

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["my8mw4-3000.csb.app"],
+  images: {
+    minimumCacheTTL: 31536000,
+    remotePatterns: [],
+  },
   compiler: {
     removeConsole:
       process.env.NODE_ENV === "production"
