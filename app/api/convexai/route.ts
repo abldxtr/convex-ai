93b335d7068fb17780a99a44b9bb0b17c0fa1e07
abol.sa.lej <abol.dexter@gmail.com>
2025-06-27 19:19:02 +0330

    bug fix

--- app/api/convexai/route.ts
+++ app/api/convexai/route.ts
@@ -101,3 +101,37 @@
 // //     // console.log(result);
 // //   },
 // // });
+
+// باید پاک شود.
+import { api } from "@/convex/_generated/api";
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
+import { fetchQuery } from "convex/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const chatId = req.nextUrl.searchParams.get("chatId");
+
+    if (!chatId) {
+      return NextResponse.json(
+        { error: "chatId is required" },
+        { status: 400 }
+      );
+    }
+
+    const token = await convexAuthNextjsToken();
+
+    const chat = await fetchQuery(
+      api.chat.getChatById,
+      { id: chatId },
+      { token }
+    );
+
+    return NextResponse.json({ chat }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
