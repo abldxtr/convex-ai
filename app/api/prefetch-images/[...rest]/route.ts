8dfab4fcded67c4238c34d0af7186c73c9100b50
abol.sa.lej <abol.dexter@gmail.com>
2025-06-27 20:16:37 +0330

    bug fix

--- app/api/prefetch-images/[...rest]/route.ts
+++ app/api/prefetch-images/[...rest]/route.ts
@@ -14,50 +14,41 @@ function getHostname() {
 }
 
 export async function GET(
-  _: NextRequest,
-  { params }: { params: { rest: string[] } }
+  request: NextRequest,
+  context: { params: { rest: string[] } }
 ) {
   const schema = process.env.NODE_ENV === "development" ? "http" : "https";
   const host = getHostname();
-  // const host = "fksxn3-3000.csb.app";
 
-  console.log("host", host);
   if (!host) {
     return new Response("Failed to get hostname from env", { status: 500 });
   }
-  // const href = (await params).rest.join("/");
-  const href = params.rest.join("/");
 
-  console.log("href", href);
+  const href = context.params.rest.join("/");
 
   if (!href) {
     return new Response("Missing url parameter", { status: 400 });
   }
 
   const url = `${schema}://${host}/${href}`;
-  console.log("url", url);
-
-  // for disable prefetch
-
-  // if (url) {
-  //   return;
-  // }
 
   const response = await fetch(url);
   if (!response.ok) {
     return new Response("Failed to fetch", { status: response.status });
   }
+
   const body = await response.text();
   const { document } = parseHTML(body);
   const images = Array.from(document.querySelectorAll("main img"))
     .map((img: any) => ({
-      srcset: img.getAttribute("srcset") || img.getAttribute("srcSet"), // Linkedom is case-sensitive
+      srcset: img.getAttribute("srcset") || img.getAttribute("srcSet"),
       sizes: img.getAttribute("sizes"),
       src: img.getAttribute("src"),
       alt: img.getAttribute("alt"),
       loading: img.getAttribute("loading"),
     }))
     .filter((img) => img.src);
+
   return NextResponse.json(
     { images },
     {
