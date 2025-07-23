c8f1b01a0155df917f7cc34fedd02fe66792bd8b
abol.sa.lej <abol.dexter@gmail.com>
2025-07-23 05:36:16 +0330

    add force static

--- app/(main)/chat/page.tsx
+++ app/(main)/chat/page.tsx
@@ -1,5 +1,7 @@
 import ChatClientWithoutId from "@/components/chat-client-without-id";
 
+export const dynamic = "force-static";
+
 export default async function Chat() {
   return <ChatClientWithoutId />;
 }
