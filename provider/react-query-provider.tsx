cf110c7fbe96ad0b01dd06dc9750fded4d13df4e
abol.sa.lej <abol.dexter@gmail.com>
2025-07-26 03:03:36 +0330

    add react qeury devTools

--- provider/react-query-provider.tsx
+++ provider/react-query-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 // import { getQueryClient } from '@/app/get-query-client'
 import type * as React from "react";
 import { getQueryClient } from "./get-query-client";
@@ -12,6 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       {children}
       {/* <ReactQueryDevtools /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
