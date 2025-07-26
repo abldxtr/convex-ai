67e6fa815631ddab2cb86a42103bb4ae930653a6
abol.sa.lej <abol.dexter@gmail.com>
2025-07-26 03:43:43 +0330

    remove react scan

--- lib/font.tsx
+++ lib/font.tsx
@@ -1,11 +1,13 @@
 import { Geist, Vazirmatn } from "next/font/google";
 
+export const vazirmatn = Vazirmatn({
+  subsets: ["arabic"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-vazirmatn",
+  preload: true,
+});
 export const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
-});
-
-export const vazirmatn = Vazirmatn({
-  subsets: ["arabic"],
-  variable: "--font-vazirmatn",
 });
