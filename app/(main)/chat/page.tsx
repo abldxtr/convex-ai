af41ecda7805cd607a9ea46aad06ea9fe69378b6
abol.sa.lej <abol.dexter@gmail.com>
2025-10-02 17:09:00 +0330

    add static render for homepage

--- app/(main)/chat/page.tsx
+++ app/(main)/chat/page.tsx
@@ -1,6 +1,6 @@
 import ChatClientWithoutId from "@/components/chat-client-without-id";
 
-// export const dynamic = "force-static";
+export const dynamic = "force-static";
 
 export default async function Chat() {
   return <ChatClientWithoutId />;
